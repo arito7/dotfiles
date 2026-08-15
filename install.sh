b648ad12da4d19ad1679d7d84a547eba56642923
yuji <accmanager1990@gmail.com>
2026-08-14 22:39:13 -0400

    t

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ files=(
   "nvim:.config/nvim"
   "tmux.conf:.config/tmux/tmux.conf"
   "gitui_keybindings.ron:.config/gitui/key_bindings.ron"
-  "oh-my-bash:.config/oh-my-bash"
 )
 
 echo "Starting dotfiles symlinking..."
